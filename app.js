bc34baa7bd434e8a8e5a63615c8087dea25b3dd1
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-30 12:38:02 -0600

    agregando función para agregar un amigo y crear una lista con cada uno

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+const nuevoAmigo = document.getElementById('amigo')
+const listaAmigos = document.getElementById('listaAmigos');
+
+
+const agregarAmigo = () => {
+    if (nuevoAmigo.value === "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nuevoAmigo.value)
+        // listaAmigos.innerText = amigos
+        nuevoAmigo.value=""
+    }
+
+    console.log(amigos)
+
+    // Limpiamos la lista antes de volver a generarla
+    listaAmigos.innerHTML = "";
+
+
+    amigos.forEach(amigo => {
+        const li =document.createElement('li');
+        li.innerText = amigo
+        listaAmigos.appendChild(li)
+    })
+}
