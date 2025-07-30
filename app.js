12819599b51741873336e0611362f1d74d73cf59
JesusPineda29 <jesuspineda1429@gmail.com>
2025-07-30 13:02:40 -0600

    agregando función sortear de forma aleatoria un amigo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let amigos = [];
 const nuevoAmigo = document.getElementById('amigo')
 const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
 
 
 const agregarAmigo = () => {
@@ -13,15 +14,28 @@ const agregarAmigo = () => {
         nuevoAmigo.value=""
     }
 
-    console.log(amigos)
-
     // Limpiamos la lista antes de volver a generarla
     listaAmigos.innerHTML = "";
-
 
     amigos.forEach(amigo => {
         const li =document.createElement('li');
         li.innerText = amigo
         listaAmigos.appendChild(li)
     })
+    console.log(amigos)
+}
+
+
+const sortearAmigo = () => {
+    if(amigos.length <= 1 ) {
+        alert("No hay amigos para sortear, agrega almenos 2 amigos")
+    } else {
+        const numero = Math.floor(Math.random() * amigos.length);
+        console.log(numero)
+        console.log(`El amigo secreto es: ${amigos[numero]}`)
+        const li = document.createElement('li')
+        li.innerText = `El amigo secreto es: ${amigos[numero]}`
+        resultado.innerHTML=''
+        resultado.appendChild(li)
+    }
 }
